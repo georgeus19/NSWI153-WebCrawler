54eca45ceccb1ba6fa240d32bc20acffc801dc0e
Matyáš Brabec <matyas.brabec@profinit.eu>
2023-06-08 11:21:28 +0200

    add tslint

--- source/backend/src/index.ts
+++ source/backend/src/index.ts
@@ -1,15 +1,13 @@
-import express, { Express} from "express";
+import express, { Express } from "express";
 
 const app: Express = express();
 
 const port = 3000;
 
 app.get("/", async (req: any, res: any) => {
-    res.send("ahoj");
-  });
+  res.send("ahoj");
+});
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${port}`);
 });
-        
-      
