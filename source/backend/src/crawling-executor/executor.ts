fe116965de97023c7298af9c72e833adf670adfb
Krystof Hruby <hrubyk97@gmail.com>
2023-09-10 10:12:49 +0200

    add execution check.

--- source/backend/src/crawling-executor/executor.ts
+++ source/backend/src/crawling-executor/executor.ts
@@ -1,6 +1,54 @@
+import { v4 as uuidv4 } from 'uuid';
+import { Collection, MongoClient } from 'mongodb';
+import workerpool from 'workerpool';
+import { WebsiteRecord } from '../website-record';
+import { Redis, RedisOptions } from 'ioredis';
+import { FinishedCrawlingExecution } from './crawling-execution';
+import { getWebsiteRecordsCollection } from '../db-access';
 
-export function createCrawlingExecutor() {
+type PendingExecution = WebsiteRecord & {executionId: string};
+export function createCrawlingExecutor(mongoClient: MongoClient, redisOptions: RedisOptions) {
+    const pool = workerpool.pool(__dirname + '/execution-worker.js');
+    const recordsCollection = getWebsiteRecordsCollection(mongoClient);
+    const redis = new Redis(redisOptions);
+    const executions: PendingExecution[] = [];
+    
+    function addExecution(executionId: string, websiteRecord: WebsiteRecord) {
+        executions.push({executionId: executionId, ...websiteRecord});
+    }
 
+    async function runExecution(executionId: string, websiteRecord: WebsiteRecord) {
+        const execution = (await pool.exec('runCrawlingExecution', [
+            {
+                executionId: executionId,
+                url: websiteRecord.url,
+                boundaryRegexp: websiteRecord.boundaryRegExp,
+                redisOptions: redisOptions,
+            },
+        ])) as FinishedCrawlingExecution;
+        const existingKeysCount = await redis.exists(executionId);
+        if (existingKeysCount > 0) {
 
-    return {};
+        }
+
+        console.log('execution return', execution);
+    }
+
+    
+
+    // pool.terminate();
+
+    return {
+        addExecution,
+        runExecution
+    };
 }
+
+// const x = await pool.exec('runCrawlingExecution', [
+//     {
+//         executionId: 'executionCrawlRecords-' + uuidv4(),
+//         url: 'https://www.zelezarstvizizkov.cz/',
+//         boundaryRegexp: '^http.*',
+//         redisOptions: redisOptions,
+//     },
+// ]);
--- source/backend/src/main.ts
+++ source/backend/src/main.ts
@@ -3,6 +3,7 @@ import { MongoClient } from 'mongodb';
 import { addWebsiteRecordsApi } from './api/website-records';
 import bodyParser from 'body-parser';
 import { addCrawlExecutionsApi } from './api/crawl-executions';
+import { createCrawlingExecutor } from './crawling-executor/executor';
 
 const mongoUri = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.5';
 const mongoClient = new MongoClient(mongoUri);
@@ -14,25 +15,9 @@ app.use(bodyParser.json());
 addWebsiteRecordsApi(app, mongoClient);
 addCrawlExecutionsApi(app, mongoClient);
 
-app.listen(3000, () => {
-    console.log('App listening on port 3000');
-});
+// app.listen(3000, () => {
+//     console.log('App listening on port 3000');
+// });
 
-
-// import { v4 as uuidv4 } from 'uuid';
-// import workerpool from 'workerpool';
-// async function main() {
-//     const pool = workerpool.pool(__dirname + '/execution-worker.js');
-//     await pool.exec('runCrawlingExecution', [
-//         {
-//             executionId: 'executionCrawlRecords-' + uuidv4(),
-//             url: 'https://www.zelezarstvizizkov.cz/',
-//             boundaryRegexp: '^http.*',
-//             redisOptions: 6379,
-//         },
-//     ]);
-
-//     pool.terminate();
-// }
-
-// main();
+const exe = createCrawlingExecutor();
+exe.runExecution();
